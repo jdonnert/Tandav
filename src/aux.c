5cbe7cc62c58b87657dfbafa076694d1cb860491
Julius Donnert <donnert@ira.inaf.it>
2013-08-01 02:13:14 +0200

    test

--- src/aux.c
+++ src/aux.c
@@ -1,33 +1,8 @@
 #include "globals.h"
 
-
-/* Create an MPI Communicator from a range of ranks */
-inline MPI_Comm Create_MPI_Communicator(const int firstTask, const int lastTask)
-{
-	const int mpi_range[1][3] = {readTask, lastTask, 1}; 
-	
-	MPI_Group mpi_world_group, mpi_read_group;
-	
-	MPI_Comm mpi_comm_group;
-	
-	MPI_Comm_group(MPI_COMM_WORLD, &mpi_world_group); 
-	
-	MPI_Group_range_incl(worldGroup, 1, mpi_range, &mpi_read_group);
-	
-	MPI_Comm_create(MPI_COMM_WORLD, mpi_read_group, &mpi_comm_read_group);
-	
-	MPI_Group_free(mpi_world_group);
-	
-	MPI_Group_free(mpi_read_group);
-	
-	return mpi_comm_group;
-}
-
-
-
 /* Memory management */
-void *malloc_info(size_t size, const char* file, const char* func, 
-		const int line)
+void *Malloc_info(const char* file, const char* func, const int line, 
+		size_t size)
 {
 	void *result = malloc(size);
 
@@ -37,52 +12,100 @@ void *malloc_info(size_t size, const char* file, const char* func,
 	return result;
 }
 
-void *realloc_info(void *ptr, size_t size, const char* file, const char* func, 
-		const int line)
+void *Realloc_info(const char* file, const char* func, const int line, 
+		void *ptr, size_t new_size)
 {
-	void * result = realloc(ptr, size);
+	void * result = realloc(ptr, new_size);
 
-	Assert_Info(func, file, line, result != NULL || size==0,
-			"Reallocation failed: %zu bytes \n" ,size);
+	Assert_Info(func, file, line, result != NULL || new_size==0,
+			"Reallocation failed: %zu bytes \n" ,new_size);
 
 	return (result);
 }
 
-void safe_free(void *ptr) 
+void Free_info(const char* file, const char* func, const int line, 
+		void *ptr) 
 {
     if (ptr != NULL)        
     	free(ptr);
-    
+
+	else
+		printf("WARNING Task %d. You tried to free a NULL pointer "
+				"in file %s, function %s(), line %d\n", 
+				Task.Rank, file, func, line);
     return;
 }
 
 /* Reallocates the Particle structures. Takes the relative change
- * as argument, not the total number. Add or Remove via sign argument.
- * Also updates Task.Npart and Task.NPartTotal */
-void Reallocate_P(size_t nPart[NO_PART_TYPES], int sign)
+ * as argument, not the total number. Add or Remove via sign of nPart.
+ * Also updates Task.Npart and Task.NPartTotal. 
+ * Expands P so that space for nPart[type] is at offset[type]
+ * Contracts P so that the last nPart[type] particles are removed */
+void Reallocate_P_Info(const char *func, const char *file, int line, 
+		int dNpart[NO_PART_TYPES], int offset_out[NO_PART_TYPES])
 {
-	for (int type = 0; type < NO_PART_TYPES; type++) {
-		Task.Npart[type] += sign * nPart[type];
-		Task.NpartTotal += sign * nPart[type];
+	size_t offset[NO_PART_TYPES] = { 0 };
+	ptrdiff_t new_npart[NO_PART_TYPES] = { 0 };
+	size_t new_npartTotal = 0;
+	
+	for (int type = 0; type < NO_PART_TYPES; type++) { // calc offset
 
-        Assert(Task.Npart[type]>=0, "Can't allocate negative particles");
+		new_npart[type] = Task.Npart[type] + dNpart[type];
+		
+		new_npartTotal += new_npart[type];
+		
+        Assert(new_npart[type] >= 0, 
+			"Can't alloc negative particles, type %d, delta %d, current %d,\n"
+			"requested from %s, %s(), line %d", 
+			type, dNpart[type], Task.Npart[type], file, func, line);
+
+		if (dNpart[type]) 
+			continue; // don't need offset here
+
+		for (int i=0; i <= type; i++)
+			offset[type] += new_npart[i];
+
+		offset[type] -= max(0, dNpart[type]); // correct for dNpart > 0 
 	}
 
-    Assert(Task.NpartTotal>=0, "Can't allocate negative particles");
+	for (int type = 0; type < NO_PART_TYPES; type++) { // move left
 
-	size_t nBytes = sizeof(*P)*Task.NpartTotal;
+		size_t src = offset[type] - dNpart[type]; 
+		size_t dest = offset[type];
+		ptrdiff_t npart = Task.NpartTotal - src;
 
-	P = realloc(P, nBytes);
+		if (dNpart[type] < 0 && npart > 0)
+			memmove(&P[dest], &P[src], npart*sizeof(*P));
+	}
 
-	Assert(P!=NULL, "Particle Reallocation failed");
+	size_t nBytes = sizeof(*P) * new_npartTotal;
+	P = Realloc(P, nBytes);
 
-	return;
+	for (int type = 0; type < NO_PART_TYPES-1; type++) { // move right
+
+		size_t src = offset[type];
+		size_t dest = offset[type] + dNpart[type];
+		ptrdiff_t npart = Task.NpartTotal - src;
+
+		if (dNpart[type] > 0 && npart > 0)
+			memmove(&P[dest], &P[src], npart*sizeof(*P));
+	}
+
+	Task.NpartTotal = new_npartTotal;
+	
+	for (int type = 0; type < NO_PART_TYPES; type++) // book keeping
+		Task.Npart[type] = new_npart[type];
+
+	if (offset_out != NULL) // return ptrs to freed space
+		memcpy(offset_out, offset, NO_PART_TYPES);
+		
+	return ;
 }
 
 /* Error Handling, we use variable arguments to be able
  * to print more informative error messages */
-void Assert_Info(const char *func, const char *file, int line, int expr, 
-		const char *errmsg, ...)
+void Assert_Info(const char *func, const char *file, int line,
+		int expr, const char *errmsg, ...)
 {
     if (expr)
         return;
@@ -92,8 +115,8 @@ void Assert_Info(const char *func, const char *file, int line, int expr,
 	va_start(varArgList, errmsg);
 
 	/* we fucked up, tell them */
-    fprintf(stderr, "\nTask %d: ERROR %s:%d : %s : \n	", Task.Rank, 
-			file, line, func);
+    fprintf(stderr, "\nERROR Task %d: In file %s, function %s(), line %d :\n\n", 
+			Task.Rank, file, func, line);
 
 	vfprintf(stderr, errmsg, varArgList); 
 	

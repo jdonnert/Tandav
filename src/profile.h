7a0fe7a38d83ecf965ce8134098b6769c37e4e6f
Julius Donnert <donnert@ira.inaf.it>
2014-01-27 09:37:21 +0100

    Small changes

--- src/profile.h
+++ src/profile.h
@@ -2,7 +2,7 @@ void Init_Profiler();
 void Finish_Profiler();
 void Profile_Info(const char* file, const char* func, const int line, 
 		const char *name);
-
 void Profile_Report();
 void Profile_Report_Last();
 void Write_Logs();
+float Runtime();
